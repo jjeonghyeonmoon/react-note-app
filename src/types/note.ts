0fcc581021ea367d4077b74ab4c20b1c38aa3e1e
jjeonghyeonmoon <answjdgus72@naver.com>
2025-11-23 15:48:48 +0900

    interface

---
+++ src/types/note.ts
@@ -0,0 +1,15 @@
+import type { Tag } from "./tag";
+
+export interface Note {
+    title: string;
+    content: string;
+    tags: Tag[];
+    color: string;
+    priority: string;
+    isPinned: boolean;
+    isRead: boolean;
+    date: string;
+    createdTime: number;
+    editedTime: null | number;
+    id: string;
+}
